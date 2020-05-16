bf3e871dba9a5e978c2a22401a73816793d48a5b
RohYounwoo <ywroh0619@naver.com>
2020-05-16 14:11:06 +0900

    googleMapAPI_test

---
+++ test_code_2/src/test_code_2/Main.java
@@ -0,0 +1,22 @@
+package test_code_2;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Main extends JFrame{
+	private GoogleAPI googleAPI = new GoogleAPI();
+	private String location = "¡¶¡÷µµ";
+	private JLabel googleMap;
+	
+	
+    public Main() {
+    	googleAPI.downloadMap(location);
+    	googleMap = new JLabel(googleAPI.getMap(location));
+    	googleAPI.fileDelete(location);
+    	add(googleMap);
+    	
+        setTitle("Google Maps");
+        setVisible(true);
+        pack();
+    }
+}
