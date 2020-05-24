f94bc6d6df200f24143b576a2b8fb8d852c4755b
unknown <chavi9116@gmail.com>
2020-05-24 21:44:08 +0900

    createMap

--- makingGUI/src/makingGUI/Frame.java
+++ makingGUI/src/makingGUI/Frame.java
@@ -1,23 +1,42 @@
 package makingGUI;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Container;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Frame extends JFrame {
-
-	Frame frame = new Frame();
 	
-	public void createFrame() {
-		super("takingTayoWithBomBom");
-		frame.setSize(500, 500);
-		frame.setVisible(true);
+	Container cPane;
+	ImageIcon img;
+	JLabel Imgbox;
+	Frame frame = new Frame();
+	final int FRAME_WIDTH = img.getIconWidth();
+	final int FRAME_HEIGHT = img.getIconHeight();
+	
+	//JButton btn1 = new JButton("Á¦´ë");
+	
+	public Frame() {
+		frame.setTitle("takingTayoWithBomBom");
+		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		//		super("takingTayoWithBomBom");
 		
+		cPane = getContentPane();
+		cPane.setLayout(null);
+		
+		img = new ImageIcon(".\\img\\jejuMap.jpg");
+		Imgbox = new JLabel(img);
+		Imgbox.setBounds(0, 50, img.getIconWidth(), img.getIconHeight());
+		
+		cPane.add(Imgbox);
 	}
 
 	public static void main(String[] args) {
-		Frame frame = new Frame();
-		frame.createFrame();
+		new Frame();
 
 	}
 
