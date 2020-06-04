46135a54475bf54bb770fa36c8cb7997a662557e
RohYounwoo <ywroh0619@naver.com>
2020-06-04 21:42:19 +0900

    merge

--- makingGUI/src/makingGUI/Frame.java
+++ makingGUI/src/makingGUI/Frame.java
@@ -11,7 +11,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 
 public class Frame extends JFrame {
 	Mypanel panel = new Mypanel();
