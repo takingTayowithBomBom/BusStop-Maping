5c2901dfed3a05f44a05cb22e3d310a90bfc8604
RohYounwoo <ywroh0619@naver.com>
2020-06-29 23:33:35 +0900

    frame

--- makingGUI/src/makingGUI/ClientSocket.java
+++
@@ -1,9 +0,0 @@
-package makingGUI;
-
-import java.net.Socket;
-
-public class ClientSocket {
-	public static void main(String[] args) {
-		Socket clientSocket = new Socket("123.456.0.1",8888);
-	}
-}
